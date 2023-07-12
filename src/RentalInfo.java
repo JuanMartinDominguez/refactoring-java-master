2f525206c7768211a8d3fffc83b3fab09e1e55d3
JuanMartinDominguez <85463091+JuanMartinDominguez@users.noreply.github.com>
2023-07-12 18:40:22 -0500

    Refact. Extract Method
    Se aplicó la técnica de refactorización Extract Method para reducir la complejidad cognitiva del método "public String statement(Customer customer)" de la clase "RentalInfo.java" y que el código de dicho método sea de mayor entendimiento cuando se tenga que mantener el software. La herramienta SonarQube indica que su complejidad era de 16, cuando se sugiere que sea menor o igual a 15.

--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -2,49 +2,68 @@ import java.util.HashMap;
 
 public class RentalInfo {
 
-  public String statement(Customer customer) {
-    HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
+	public String statement(Customer customer) {
+	    HashMap<String, Movie> movies = createMoviesMap();
+	    
+	    double totalAmount = 0;
+	    int frequentEnterPoints = 0;
+	    StringBuilder result = new StringBuilder("Rental Record for ").append(customer.getName()).append("\n");
+	    
+	    for (MovieRental r : customer.getRentals()) {
+	        double thisAmount = calculateAmount(r, movies);
+	        frequentEnterPoints += calculateFrequentEnterPoints(r, movies);
 
-    double totalAmount = 0;
-    int frequentEnterPoints = 0;
-    String result = "Rental Record for " + customer.getName() + "\n";
-    for (MovieRental r : customer.getRentals()) {
-      double thisAmount = 0;
+	        //print figures for this rental
+	        result.append("\t").append(movies.get(r.getMovieId()).getTitle()).append("\t").append(thisAmount).append("\n");
+	        totalAmount += thisAmount;
+	    }
 
-      // determine amount for each movie
-      if (movies.get(r.getMovieId()).getCode().equals("regular")) {
-        thisAmount = 2;
-        if (r.getDays() > 2) {
-          thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
-        }
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("new")) {
-        thisAmount = r.getDays() * 3;
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("childrens")) {
-        thisAmount = 1.5;
-        if (r.getDays() > 3) {
-          thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
-        }
-      }
+	    // add footer lines
+	    result.append("Amount owed is ").append(totalAmount).append("\n");
+	    result.append("You earned ").append(frequentEnterPoints).append(" frequent points\n");
 
-      //add frequent bonus points
-      frequentEnterPoints++;
-      // add bonus for a two day new release rental
-      if (movies.get(r.getMovieId()).getCode() == "new" && r.getDays() > 2) frequentEnterPoints++;
+	    return result.toString();
+	}
 
-      //print figures for this rental
-      result += "\t" + movies.get(r.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
-      totalAmount = totalAmount + thisAmount;
-    }
-    // add footer lines
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentEnterPoints + " frequent points\n";
+	private HashMap<String, Movie> createMoviesMap() {
+	    HashMap<String, Movie> movies = new HashMap<>();
+	    String regular = "regular";
+	    movies.put("F001", new Movie("You've Got Mail", regular));
+	    movies.put("F002", new Movie("Matrix", regular));
+	    movies.put("F003", new Movie("Cars", "childrens"));
+	    movies.put("F004", new Movie("Fast & Furious X", "new"));
+	    return movies;
+	}
 
-    return result;
-  }
+	private double calculateAmount(MovieRental rental, HashMap<String, Movie> movies) {
+	    Movie movie = movies.get(rental.getMovieId());
+	    double thisAmount = 0;
+
+	    if (movie.getCode().equals("regular")) {
+	        thisAmount = 2;
+	        if (rental.getDays() > 2) {
+	            thisAmount += (rental.getDays() - 2) * 1.5;
+	        }
+	    } else if (movie.getCode().equals("new")) {
+	        thisAmount = rental.getDays() * 3.00;
+	    } else if (movie.getCode().equals("childrens")) {
+	        thisAmount = 1.5;
+	        if (rental.getDays() > 3) {
+	            thisAmount += (rental.getDays() - 3) * 1.5;
+	        }
+	    }
+
+	    return thisAmount;
+	}
+
+	private int calculateFrequentEnterPoints(MovieRental rental, HashMap<String, Movie> movies) {
+	    Movie movie = movies.get(rental.getMovieId());
+	    int frequentEnterPoints = 1;
+
+	    if (movie.getCode().equals("new") && rental.getDays() > 2) {
+	        frequentEnterPoints++;
+	    }
+
+	    return frequentEnterPoints;
+	}
 }
